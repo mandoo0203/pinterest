83d65706bb52b89ba639c9227a460472622bb601
민수의 iMacbook <mandoo0203@minsuui-MacBook-Air.local>
2016-09-23 17:36:17 +0900

    cardView & responsive

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923073646) do
+ActiveRecord::Schema.define(version: 20160923081031) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "user_id"
+    t.string   "pin_image_file_name"
+    t.string   "pin_image_content_type"
+    t.integer  "pin_image_file_size"
+    t.datetime "pin_image_updated_at"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
